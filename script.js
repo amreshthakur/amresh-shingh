d40d72e778855e4f126d0040d954c31ffac5d624
Amresh Thakur <amresh.csit@gmail.com>
2024-06-12 10:00:17 +0545

    updates

--- script.js
+++ script.js
@@ -5,3 +5,13 @@ document.getElementById('toggle-button').addEventListener('click', function() {
     menu.style.display = (menu.style.display === 'flex') ? 'none' : 'flex';
     socialIcons.style.display = (socialIcons.style.display === 'flex') ? 'none' : 'flex';
 });
+
+
+// -----------------------------scroll of Our Valuable Clients --------------------
+const scrollContainer = document.querySelector('.scroll-container');
+const scrollContent = document.querySelector('.scroll-content');
+
+// Clone the content
+// const clone = scrollContent.cloneNode(true);
+// scrollContainer.appendChild(clone);
+// -----------------------------scroll of Our Valuable Clients end code --------------------
